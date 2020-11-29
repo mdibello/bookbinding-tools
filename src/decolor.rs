d96ad582226e86b9fb42aeaaf3ddc4f50a197a49
Matthew DiBello <matthew.quickbeam@gmail.com>
2020-11-29 14:10:17 -0500

    Minor modfication, and add Windows support

--- src/decolor.rs
+++ src/decolor.rs
@@ -1,37 +1,52 @@
 use std::process::Command;
 use std::fs;
+use std::convert::Into;
 
 pub fn decolor(input_filename: String) {
 
     // convert PDF to PNG
     if cfg!(target_os = "windows") {
-        panic!("Cannot run on Windows, needs access to Image Magick");
+        let _ = Command::new("imagemagick")
+                        .arg("convert")
+                        // .arg("-density")
+                        // .arg("300")
+                        .arg(input_filename)
+                        .arg("-colorspace")
+                        .arg("Gray")
+                        .arg("-quality")
+                        .arg("100")
+                        .arg("split/orig-page-%04d.png")
+                        .output()
+                        .expect("Failed to split PDF and convert to PNG");
+    } else {
+        let _ = Command::new("convert")
+                        // .arg("-density")
+                        // .arg("300")
+                        .arg(input_filename)
+                        .arg("-colorspace")
+                        .arg("Gray")
+                        .arg("-quality")
+                        .arg("100")
+                        .arg("split/orig-page-%04d.png")
+                        .output()
+                        .expect("Failed to split PDF and convert to PNG");
     }
 
-    // fs::remove_dir_all("split");
+    fs::remove_dir_all("split");
     fs::create_dir("split");
 
-    let _ = Command::new("convert")
-                    .arg("-density")
-                    .arg("300")
-                    .arg(input_filename)
-                    //.arg("-colorspace")
-                    //.arg("Gray")
-                    .arg("-quality")
-                    .arg("100")
-                    .arg("split/page-%04d.png")
-                    .output()
-                    .expect("Failed to split PDF and convert to PNG");
 
     let pages = fs::read_dir("split").unwrap();
     for page in pages {
+
         let page_filename = page.unwrap().file_name().into_string().unwrap();
         let img = image::io::Reader::open(format!("split/{}", page_filename))
                                         .expect("Failed to open image")
                                         .decode()
                                         .expect("Failed to decode image");
+
         let greyscale_img = img.into_luma8();
-        fs::write("output.txt", format!("{:?}", greyscale_img));
+        // fs::write("output.txt", format!("{:?}", greyscale_img));
 
         let mut pixels: Vec<u8> = Vec::new();
         for p in greyscale_img.pixels() {
@@ -42,6 +57,10 @@ pub fn decolor(input_filename: String) {
         }
 
         profile_pixels(pixels);
+
+        // let img_buf: [u8] = pixels.into();
+
+        // image::ImageEncoder::write_image(&pixels.into(), )
     }
 
 }
